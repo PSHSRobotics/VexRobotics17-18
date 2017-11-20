ff86a6ba749076eae7e66ee214e3745c46bc93e2
PSHSRobotics <33577367+PSHSRobotics@users.noreply.github.com>
2017-11-20 14:58:57 -0600

    Add files via upload

--- robotB.c
+++ robotB.c
@@ -11,27 +11,59 @@
 // left ladder port4
 // right ladder port 5
 void moveForward(int speed){
-	setMotor(port1);
-	setMotor(port2);
+	setMotor(port2, -1 * speed);
+	setMotor(port3, speed);
 	forward(speed);
-	wait(1);
-	stopMotor(port1);
 	stopMotor(port2);
-
+	stopMotor(port3);
 }
 
 void moveBackward(int speed){
-	setMotor(port1);
-	setMotor(port2);
+	setMotor(port2, -1 * speed);
+	setMotor(port3, speed);
 	forward(-1 * speed);
-	wait(1);
-	stopMotor(port1);
 	stopMotor(port2);
+	stopMotor(port3);
+}
+void right(int speed){
+	setMotor(port2, speed);
+	forward(speed);
+	stopMotor(port2);
+}
 
+void left(int speed){
+	setMotor(port3, -1 * speed);
+	forward(-1 * speed);
+	stopMotor(port3);
 }
 
 task main()
 {
+		int isForward = 1;
+	int speed = 10;
+	while(1==1){
+		if(vexRT[Btn8U] == 1){
+			moveForward(speed);
+			if(speed <= 127){
+				speed += 1;}
+			}
 
+  	if(vexRT[Btn6D] == 1){
+			stopAllMotors();
+			speed = 10;
+			}
+
+		if(vexRT[Btn8D] == 1){
+			moveBackward(-1 * speed);
+			if(speed <= 127){
+				speed += 1;
+			}
+		}
+		if(vexRT[Btn8L] == 1){
+			left(-127);
+		}
+		if(vexRT[Btn8R] == 1){
+			right(-127);
+		}
 
 }
